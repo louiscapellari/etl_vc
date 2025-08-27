9bc5854cb4c66334cd7efb77eff734736511ab7c
louiscapellari <capellarilouis@gmail.com>
2025-08-27 10:23:10 +0200

    Delete run_pipeline_update.bat

--- run_pipeline_update.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "Renseignez le chemin du dossier acceuillant les scripts ici"
-REM Active l'environnement Conda
-CALL C:\ProgramData\anaconda3\Scripts\activate.bat "<= chemin pour activer l'environnement python (supprimer ce qu'il y a entre parenthèses" C:\Users\LouisPC\.conda\envs\sig_vc_test "<= chemin de l'environnement python"
-python pipeline.py --update
-pause
